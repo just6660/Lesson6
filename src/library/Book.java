98743b846ff204aa48d506233c68d5a29c4d81b7
just6660 <just6660@ISS2TZQKH2.scdsb.pri>
2020-10-02 13:15:14 -0400

    done library

--- src/library/Book.java
+++ src/library/Book.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package library;
 
-/**
- *
- * @author just6660
- */
 public class Book {
+    private String title, author;
+    
+    public Book(String t, String a){
+        title = t;
+        author = a;
+    }
+    
+    public String toString(){
+        String str;
+        str = (title + "| By: " + author);
+        return str;
+    }
+    public String getTitle(){
+        return title; 
+    }
     
 }
--- src/library/Patron.java
+++ src/library/Patron.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package library;
 
-/**
- *
- * @author just6660
- */
 public class Patron {
+    private String name;
+    Book book1,book2,book3;
+    
+    
+    public Patron(String n){
+        name = n;
+        book1 = null;
+        book2 = null;
+        book3 = null;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public boolean Borrow(Book b){
+        if(book1 == null){
+            book1 = b;
+            return true;
+        }
+        else if(book2 == null){
+            book2 = b;
+            return true;
+        }
+        else if(book3 == null){
+            book3 = b;
+            return true;
+        }
+        else
+            return false;
+        
+    }
+    
+    public boolean returnBook(Book b){
+        if(book1==b){
+            book1 = null;
+            return true;
+        }
+        else if(book2 == b){
+            book2 = null;
+            return true;
+        }
+        else if(book3 == b){
+            book3 = null;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public boolean hasBook(Book b){
+        if(book1 == b || book2 == b || book3 == b)
+            return true;
+        else
+            return false;
+    }
+    
+    
     
 }
